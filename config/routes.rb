2e54cb30ebb6e53420c6c9fff1fec7f60fece0a1
Rodaan <rodaan.rabang@gmail.com>
2018-03-04 19:23:39 -0800

    Fix issue

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   get 'test', to: 'welcome#testemail'
-  post 'test', to: 'welcome#testingemail'
+  post 'cool', to: 'welcome#testingemail'
 end
